0c342b9bde51fad34933d48a7dd95ca279b6cdc8
Keshyu <asdfsdev264@gmail.com>
2024-01-16 00:31:15 +0300

    .

--- grammar.js
+++ grammar.js
@@ -1,6 +1,6 @@
 module.exports = grammar({
   name: 'bismuth',
-  extras: $ => [$._space],
+  extras: $ => [/[ \f\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/],
   word: $ => $.tree_sitter_word,
   rules: {
     source_file: $ => $._sequence,
@@ -16,24 +16,14 @@ module.exports = grammar({
       $.group,
       $.pipe,
       alias($.comma_group, $.group),
-      alias($.dot_pipe, $.pipe),
       $.call,
+      alias($.dot_pipe, $.pipe),
+      alias($.no_space_call, $.call),
       $.word,
     ),
 
     group: $ => seq('(', $._sequence, ')'),
     pipe: $ => seq('{', $._sequence, '}'),
-
-    call: $ => seq(
-      field('fn', $._expr_call),
-      field('input', repeat1($._expr_call)),
-    ),
-    _expr_call: $ => choice(
-      $.group,
-      $.pipe,
-      alias($.dot_pipe, $.pipe),
-      $.word,
-    ),
 
     comma_group: $ => seq(
       optional(repeat1(',')),
@@ -43,8 +33,21 @@ module.exports = grammar({
     _expr_comma_group: $ => choice(
       $.group,
       $.pipe,
-      alias($.dot_pipe, $.pipe),
       $.call,
+      alias($.dot_pipe, $.pipe),
+      alias($.no_space_call, $.call),
+      $.word,
+    ),
+
+    call: $ => seq(
+      field('fn', $._expr_call),
+      field('input', alias(repeat1($._expr_call), $.group)),
+    ),
+    _expr_call: $ => choice(
+      $.group,
+      $.pipe,
+      alias($.dot_pipe, $.pipe),
+      alias($.no_space_call, $.call),
       $.word,
     ),
 
@@ -54,14 +57,23 @@ module.exports = grammar({
     _expr_dot_pipe: $ => choice(
       $.group,
       $.pipe,
+      alias($.no_space_call, $.call),
       $.word,
+    ),
+
+    no_space_call: $ => seq(
+      field('fn', $.word),
+      field('input', alias($.immediate_group, $.group)),
+    ),
+    immediate_group: $ => seq(
+      token.immediate('('),
+      $._sequence,
+      ')',
     ),
 
     word: _ => /[a-zA-Z0-9_]+/,
     symbol: _ => /[!@#$%^&*\-=+\\|<>/?~]+/,
     tree_sitter_word: _ => /[a-zA-Z0-9_]+|[!@#$%^&*\-=+\\|<>/?~]+/,
-
-    _space: _ => /[ \f\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/,
   },
 });
 
