c2cb36d0096cfee6b27ce578af05f561f5646d09
Keshyu <asdfsdev264@gmail.com>
2024-01-18 00:22:16 +0300

    Minor stuff

--- grammar.js
+++ grammar.js
@@ -79,7 +79,7 @@ module.exports = grammar({
     ),
 
     binding: $ => seq(
-      field('id', choice($.word, $.group, $.pipe)),
+      field('id', $._expr_binding),
       /:[ \f\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/,
       field('value', $._expr_binding),
     ),
